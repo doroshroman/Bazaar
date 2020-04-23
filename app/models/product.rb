15d713759ca80e419b352fa3cbca7e16bd7f9cb3
Roman <doroshroman27@gmail.com>
2020-04-23 20:09:32 +0300

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :rating, numericality: {greater_than_or_equal_to: 0.01}
@@ -8,4 +11,14 @@ class Product < ApplicationRecord
         %r{\.(gif|jpg|png)\Z}i,
         message: 'must be a URL for GIF, JPG or PNG image.'
     }
+
+    private
+
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -52,4 +52,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:one))
+    end
+    assert_redirected_to products_url
+  end
+
 end
